4bf96344e9ce15c3d1f431ea8add04dc7e9c307a
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2020-08-04 19:51:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div>
         <Navbar bg="light">
-          <Navbar.Brand>CreateD3App</Navbar.Brand>
+          <Navbar.Brand>Scatter Plot</Navbar.Brand>
         </Navbar>
         <Container>
           <Row>
